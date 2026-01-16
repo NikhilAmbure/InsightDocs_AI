2a49beab95efe3dfd7f5e78bd60e58664246b6e4
Nikhil Ambure <nikhilambure25@gmail.com>
2026-01-16 18:22:26 +0530

    updated urls

--- InsightDocs_AI/urls.py
+++ InsightDocs_AI/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,8 +27,9 @@ urlpatterns = [
     path('', include('app.urls')),
     # Document-related pages (upload workspace, subscription, etc.)
     path('', include('documents.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
